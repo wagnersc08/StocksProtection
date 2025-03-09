e8326c2bc6eb7fe5f11daaa598e4fefedc1c9152
wagnersc08 <137009915+wagnersc08@users.noreply.github.com>
2025-03-09 17:49:10 -0300

    Add files via upload

--- StocksProtection.py
+++ StocksProtection.py
@@ -11,6 +11,8 @@ import subprocess
 import sys
 import pandas_ta as ta  # Biblioteca para indicadores técnicos
 
+!pip install setuptools
+!pip install --upgrade --force-reinstall pandas_ta
 
 # Função para gerar recomendações individuais para cada indicador
 def analyze_indicator(indicator, value, history):
